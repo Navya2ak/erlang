8a3d738d9655827fc8ad0d82d5df59d10a6187e2
navya <mail.navyaraj@gmail.com>
2024-03-28 12:30:36 +0530

    basics  of erlang

---
+++ tut.erl
@@ -0,0 +1,12 @@
+-module(tut).           %module_name:function_name(arguments)
+
+-export([double/1]).   %export the function and the no.of arguments
+
+double(X) ->           %defining the function
+    2 * X.             %ending the function with "."
+
+
+
+
+    %c(module_name).
+    %module_name:function_name(argument?).
